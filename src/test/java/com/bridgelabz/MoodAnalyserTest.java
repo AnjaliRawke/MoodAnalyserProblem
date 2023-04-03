9b24fc7c488c8bce383314e9b4ab68cf95617337
AnjaliRawke <anjalirawke21@gmail.com>
2023-04-03 20:48:26 +0530

    Added: UC2 Handle Exception like null

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -8,7 +8,7 @@ import static junit.framework.TestCase.assertEquals;
 public class MoodAnalyserTest {
 	@Test
 	public void testAnalyseMoodSad() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad Mood");
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
 		String mood = moodAnalyser.analyseMood();
 		assertEquals("SAD", mood);
 	}
@@ -29,7 +29,14 @@ public class MoodAnalyserTest {
 
 	@Test
 	public void testAnalyseMoodSadWithConstructor() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad Mood");
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
 		Assert.assertEquals("SAD", moodAnalyser.analyseMood());
+	}
+
+	@Test
+	public void testAnalyseMoodNull() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+		String mood = moodAnalyser.analyseMood();
+		assertEquals("HAPPY", mood);
 	}
 }
